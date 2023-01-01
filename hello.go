54ca3408b5075fe4528ef1e788fd525a655e566c
godprobe <godprobe.misc+github@gmail.com>
2023-01-01 14:34:51 -0800

    EFIGS support

--- hello.go
+++ hello.go
@@ -2,19 +2,25 @@ package main
 
 import "fmt"
 
-const enHello = "Hello" // English
-const esHello = "Hola"  // Spanish
+const enHello = "Hello"   // English
+const frHello = "Bonjour" // French
+const itHello = "Ciao"    // Italian
+const deHello = "Hallo"   // German
+const esHello = "Hola"    // Spanish
 
 var helloMultilingualMap = map[string]string{
 	"en": enHello,
+	"fr": frHello,
+	"it": itHello,
+	"de": deHello,
 	"es": esHello,
 }
 
 // Accepts any string for the name and an ISO 639-1 language code (as string)
 func Hello(name string, language string) string {
-	greeting := "Hello"
+	greeting := "Hello" // default greeting
 	if name == "" {
-		name = "World"
+		name = "World" // default name
 	}
 	for k, v := range helloMultilingualMap {
 		if language == k {
--- hello_test.go
+++ hello_test.go
@@ -13,6 +13,21 @@ func TestHello(t *testing.T) {
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Francesca", "fr")
+		want := "Bonjour, Francesca"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Italian", func(t *testing.T) {
+		got := Hello("Pinocchio", "it")
+		want := "Ciao, Pinocchio"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in German", func(t *testing.T) {
+		got := Hello("Hans", "de")
+		want := "Hallo, Hans"
+		assertCorrectMessage(t, got, want)
+	})
 	t.Run("in Spanish", func(t *testing.T) {
 		got := Hello("Elodie", "es")
 		want := "Hola, Elodie"
