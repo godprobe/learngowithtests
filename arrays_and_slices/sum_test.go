5abd2abb9942ef1f09c7049c1fcb90be2055657d
godprobe <godprobe.misc+github@gmail.com>
2023-01-04 22:13:18 -0800

    SumAll tests; learning about slice comparison

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -15,3 +15,22 @@ func TestSum(t *testing.T) {
 		}
 	})
 }
+
+func TestSumAll(t *testing.T) {
+	t.Run("return result for each of multiple slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+	t.Run("return result for a single slice", func(t *testing.T) {
+		got := SumAll([]int{1, 1, 1})
+		want := []int{3}
+
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
