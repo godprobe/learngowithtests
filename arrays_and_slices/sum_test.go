68324ec79259ed80c575046e12dbc5173d92a168
godprobe <godprobe.misc+github@gmail.com>
2023-01-04 22:15:31 -0800

    DeepEqual from reflect library to compare slices

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -21,7 +24,7 @@ func TestSumAll(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
 		want := []int{3, 9}
 
-		if got != want {
+		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v, want %v", got, want)
 		}
 	})
@@ -29,7 +32,7 @@ func TestSumAll(t *testing.T) {
 		got := SumAll([]int{1, 1, 1})
 		want := []int{3}
 
-		if got != want {
+		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v, want %v", got, want)
 		}
 	})
