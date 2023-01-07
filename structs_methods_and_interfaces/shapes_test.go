ea29fead0c2167fc31d74b3bd1edb1ab7d9247cd
godprobe <godprobe.misc+github@gmail.com>
2023-01-06 22:57:33 -0800

    more groundwork; basic area func, & following TDD

--- structs_methods_and_interfaces/shapes_test.go
+++ structs_methods_and_interfaces/shapes_test.go
@@ -10,3 +10,12 @@ func TestPerimeter(t *testing.T) {
 		t.Errorf("got %.2f, want %.2f", got, want)
 	}
 }
+
+func TestArea(t *testing.T) {
+	got := Area(3.25, 12.5)
+	want := 40.625
+
+	if got != want {
+		t.Errorf("got %.3f, want %.3f", got, want)
+	}
+}
