e3742347a115fbbc47d54e8d115ea4b10c630e80
godprobe <godprobe.misc+github@gmail.com>
2023-01-07 18:45:54 -0800

    Implementing an interface for the Area func

--- structs_methods_and_interfaces/shapes_test.go
+++ structs_methods_and_interfaces/shapes_test.go
@@ -13,23 +13,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g, want %g", got, want)
+		}
+	}
+
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{3.25, 12.5}
-		got := rectangle.Area()
-		want := 40.625
-
-		if got != want {
-			t.Errorf("got %.3f, want %.3f", got, want)
-		}
+		checkArea(t, rectangle, 40.625)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{5.625}
-		got := circle.Area()
-		want := 99.401955054989551685732075799078
-
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
-		}
+		checkArea(t, circle, 99.401955054989551685732075799078)
 	})
 }
