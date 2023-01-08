19234c6ed1d2e9c417d2ccb7eaf9133f1f0a6ca1
godprobe <godprobe.misc+github@gmail.com>
2023-01-07 18:28:17 -0800

    Add circle shape and use methods in lieu of funcs

--- structs_methods_and_interfaces/shapes_test.go
+++ structs_methods_and_interfaces/shapes_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{3.25, 12.5}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	want := 31.5
 
 	if got != want {
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{3.25, 12.5}
-	got := Area(rectangle)
-	want := 40.625
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{3.25, 12.5}
+		got := rectangle.Area()
+		want := 40.625
 
-	if got != want {
-		t.Errorf("got %.3f, want %.3f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.3f, want %.3f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{5.625}
+		got := circle.Area()
+		want := 99.401955054989551685732075799078
+
+		if got != want {
+			t.Errorf("got %g, want %g", got, want)
+		}
+	})
 }
