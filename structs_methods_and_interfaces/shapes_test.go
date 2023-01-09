09381771a8cd69c97ebd2e5a95ef1e98d9f4866b
godprobe <godprobe.misc+github@gmail.com>
2023-01-08 20:01:41 -0800

    Refactor: t.Run, more descriptive errors, hasArea

--- structs_methods_and_interfaces/shapes_test.go
+++ structs_methods_and_interfaces/shapes_test.go
@@ -5,28 +5,32 @@ import "testing"
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{3.25, 12.5}
 	got := rectangle.Perimeter()
-	want := 31.5
+	hasPerimeter := 31.5
 
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
+	if got != hasPerimeter {
+		t.Errorf("got %.2f, hasPerimeter %.2f", got, hasPerimeter)
 	}
 }
 
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{Width: 3.25, Height: 12.5}, want: 40.625},
-		{Circle{Radius: 5.625}, want: 99.401955054989551685732075799078},
-		{Triangle{Base: 3.25, Height: 12.5}, want: 20.3125},
+		{name: "Rectangle", shape: Rectangle{Width: 3.25, Height: 12.5}, hasArea: 40.625},
+		{name: "Circle", shape: Circle{Radius: 5.625}, hasArea: 99.401955054989551685732075799078},
+		{name: "Triangle", shape: Triangle{Base: 3.25, Height: 12.5}, hasArea: 20.3125},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g, want %g", got, tt.want)
-		}
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g, hasArea %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
