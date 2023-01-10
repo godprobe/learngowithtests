ebffc2e0c7d17ebca45664535548286a66e8cddf
godprobe <godprobe.misc+github@gmail.com>
2023-01-10 00:02:50 -0800

    Refactor; DRY it out!

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -6,31 +6,22 @@ import (
 
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(t testing.TB, wallet Wallet, want Digidollar) {
+		got := wallet.balance
+		if got != want {
+			t.Errorf("got %s, want %s", got, &want)
+		}
+	}
+
 	t.Run("Deposits", func(t *testing.T) {
 		wallet := Wallet{}
-
 		wallet.Deposit(Digidollar(10))
-
-		got := wallet.Balance()
-
-		want := Digidollar(10)
-
-		if got != want {
-			t.Errorf("got %s, want %s", got, want)
-		}
+		assertBalance(t, wallet, Digidollar(10))
 	})
 
 	t.Run("Withdrawals", func(t *testing.T) {
 		wallet := Wallet{balance: Digidollar(50)}
-
 		wallet.Withdraw(Digidollar(35))
-
-		got := wallet.Balance()
-
-		want := Digidollar(15)
-
-		if got != want {
-			t.Errorf("got %s, want %s", got, want)
-		}
+		assertBalance(t, wallet, Digidollar(15))
 	})
 }
