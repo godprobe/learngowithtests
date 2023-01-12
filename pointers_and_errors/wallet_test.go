7faea23fc521897a552cbf34d6456f9e654423ef
godprobe <godprobe.misc+github@gmail.com>
2023-01-11 22:43:50 -0800

    Covering all errors after using errcheck tool

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -8,12 +8,15 @@ func TestWallet(t *testing.T) {
 	t.Run("Deposits", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Digidollar(10))
+
 		assertBalance(t, wallet, Digidollar(10))
 	})
 
 	t.Run("Withdrawals", func(t *testing.T) {
 		wallet := Wallet{balance: Digidollar(50)}
-		wallet.Withdraw(Digidollar(35))
+		err := wallet.Withdraw(Digidollar(35))
+
+		assertNoError(t, err)
 		assertBalance(t, wallet, Digidollar(15))
 	})
 
@@ -33,6 +36,13 @@ func assertBalance(t testing.TB, wallet Wallet, want Digidollar) {
 
 	if got != want {
 		t.Errorf("got %s, want %s", got, &want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error, but didn't want one")
 	}
 }
 
