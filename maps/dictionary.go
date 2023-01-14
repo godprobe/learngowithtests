5e1b0e290f8e81134e8b5e26932fd0d9ab1ba983
godprobe <godprobe.misc+github@gmail.com>
2023-01-13 23:04:42 -0800

    Errors moved to DictionaryErr with error interface

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,17 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("not found")
-var ErrAlreadyExists = errors.New("can't add; word already exists in dictionary")
+const (
+	ErrNotFound      = DictionaryErr("could not find the word you were looking for")
+	ErrAlreadyExists = DictionaryErr("cannot add word because it already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(query string) (string, error) {
 	definition, ok := d[query]
