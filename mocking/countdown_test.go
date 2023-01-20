2ae089af4aaaa4fd01b9db4d335a148063d6a253
godprobe <godprobe.misc+github@gmail.com>
2023-01-20 00:33:33 -0800

    Confirmed failing test w/ new Sleeper/Writer Spies

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -12,6 +13,22 @@ type SpySleeper struct {
 func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
@@ -32,4 +49,23 @@ Go!`
 	if spySleeper.Calls != 3 {
 		t.Errorf("not enough calls to sleeper, want 3, got %d", spySleeper.Calls)
 	}
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v, got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
