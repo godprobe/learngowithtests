a5046f23e1384e3b6a49e188fafe4379f331595d
godprobe <godprobe.misc+github@gmail.com>
2023-01-20 00:40:47 -0800

    Testing refactored, less redundancies

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -21,20 +21,20 @@ func (d *DefaultSleeper) Sleep() {
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
-	// for i := countdownStart; i > 0; i-- {
-	// 	fmt.Fprintln(out, i)
-	// 	sleeper.Sleep()
-	// }
-	// fmt.Fprint(out, finalWord)
-	for i := countdownStart; i > 0; i-- {
-		sleeper.Sleep()
-	}
-
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		sleeper.Sleep()
 	}
-
 	fmt.Fprint(out, finalWord)
+	// for i := countdownStart; i > 0; i-- {
+	// 	sleeper.Sleep()
+	// }
+
+	// for i := countdownStart; i > 0; i-- {
+	// 	fmt.Fprintln(out, i)
+	// }
+
+	// fmt.Fprint(out, finalWord)
 }
 
 func main() {
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -6,14 +6,6 @@ import (
 	"testing"
 )
 
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
 type SpyCountdownOperations struct {
 	Calls []string
 }
@@ -31,24 +23,20 @@ const write = "write"
 const sleep = "sleep"
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
 
-	Countdown(buffer, spySleeper)
-
-	got := buffer.String()
-	want := `3
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
-
-	if spySleeper.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, want 3, got %d", spySleeper.Calls)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 
 	t.Run("sleep before every print", func(t *testing.T) {
 		spySleepPrinter := &SpyCountdownOperations{}
