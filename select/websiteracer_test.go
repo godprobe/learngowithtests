64c7f899447941370396c97be68117bf1cbcc918
godprobe <godprobe.misc+github@gmail.com>
2023-01-23 12:57:06 -0800

    Using a mock http server

--- select/websiteracer_test.go
+++ select/websiteracer_test.go
@@ -1,14 +1,27 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 // hit two sites with an HTTP GET
 // return the URL that returned first
 // if neither return after 10 seconds, return error
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
