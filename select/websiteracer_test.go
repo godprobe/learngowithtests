18490b628639b33aba0920a1dff6160271df0b77
godprobe <godprobe.misc+github@gmail.com>
2023-01-25 00:08:11 -0800

    Using the ConfigurableRacer in the testing

--- select/websiteracer_test.go
+++ select/websiteracer_test.go
@@ -12,7 +12,7 @@ import (
 // if neither return after 10 seconds, return error
 
 func TestRacer(t *testing.T) {
-	t.Run("basic slow server vs fast server test", func(t *testing.T) {
+	t.Run("compares speeds of servers, returning the url of the fastest", func(t *testing.T) {
 		slowServer := makeDelayedServer(20 * time.Millisecond)
 		fastServer := makeDelayedServer(0 * time.Millisecond)
 
@@ -23,7 +23,11 @@ func TestRacer(t *testing.T) {
 		fastURL := fastServer.URL
 
 		want := fastURL
-		got, _ := Racer(slowURL, fastURL)
+		got, err := Racer(slowURL, fastURL)
+
+		if err != nil {
+			t.Fatalf("did not expect an error, but got one %v", err)
+		}
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
@@ -31,13 +35,11 @@ func TestRacer(t *testing.T) {
 	})
 
 	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
-		serverA := makeDelayedServer(11 * time.Second)
-		serverB := makeDelayedServer(12 * time.Second)
+		server := makeDelayedServer(25 * time.Millisecond)
 
-		defer serverA.Close()
-		defer serverB.Close()
+		defer server.Close()
 
-		_, err := Racer(serverA.URL, serverB.URL)
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
 
 		if err == nil {
 			t.Error("expected an error, but didn't get one")
