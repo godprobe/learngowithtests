fd353167edee74adf74b1ec426cce5215c591982
godprobe <godprobe.misc+github@gmail.com>
2023-01-24 23:06:22 -0800

    Concurrent race via select; wait on multiple chans

--- select/websiteracer.go
+++ select/websiteracer.go
@@ -1,23 +1,21 @@
 package main
 
-import (
-	"net/http"
-	"time"
-)
+import "net/http"
 
-func Racer(urlA, urlB string) (winner string) {
-	aDuration := measureResponseTime(urlA)
-	bDuration := measureResponseTime(urlB)
-
-	if aDuration < bDuration {
-		return urlA
+func Racer(a, b string) (winner string) {
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
+		return b
 	}
-
-	return urlB
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
