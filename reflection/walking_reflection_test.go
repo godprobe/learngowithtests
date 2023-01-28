1cf58f57df80bee88a4b008819d55cd054c432b8
godprobe <godprobe.misc+github@gmail.com>
2023-01-27 23:07:34 -0800

    Iterating maps w/ MapKeys; using those w/ MapIndex

--- reflection/walking_reflection_test.go
+++ reflection/walking_reflection_test.go
@@ -86,6 +86,14 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavik"},
 		},
+		{
+			"maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
 	}
 
 	for _, test := range cases {
