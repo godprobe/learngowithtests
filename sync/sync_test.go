06e5d891d77594406b5284a8f78dcdec43fb2339
godprobe <godprobe.misc+github@gmail.com>
2023-01-30 23:22:52 -0800

    working incremental counter... not async yet

---
+++ sync/sync_test.go
@@ -0,0 +1,18 @@
+package async_counter
+
+import "testing"
+
+// Concurrency-safe counter.
+
+func TestAsyncCounter(t *testing.T) {
+	t.Run("incrementing the counter three times leaves it at 3", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		if counter.Value() != 3 {
+			t.Errorf("got %d, want %d", counter.Value(), 3)
+		}
+	})
+}
