a2754ed1f612df46731af843444fb40bf32e9133
godprobe <godprobe.misc+github@gmail.com>
2023-02-23 11:48:48 -0800

    Using ReadDir on an FS to append posts to a returned var

--- reading_files/blogposts/blogposts.go
+++ reading_files/blogposts/blogposts.go
@@ -1,3 +1,17 @@
 package blogposts
 
+import "testing/fstest"
 
+type Post struct {
+}
+
+func NewPostsFromFS(fileSystem fstest.MapFS) []Post {
+	var posts []Post
+	dir, _ := fs.ReadDir(fileSystem, ".")
+
+	for range dir {
+		posts = append(posts, Post{})
+	}
+
+	return posts
+}
