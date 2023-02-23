33f8be0dc15145f7f4fe5699793ab00628bb1258
godprobe <godprobe.misc+github@gmail.com>
2023-02-22 22:37:58 -0800

    magic number clarity... but... weird detail to it

--- maths/clockface.go
+++ maths/clockface.go
@@ -15,6 +15,15 @@ const (
 	clockCenterY     = 150
 )
 
+const (
+	secondsInHalfClock = 30
+	secondsInClock     = 2 * secondsInHalfClock
+	minutesInHalfClock = 30
+	minutesInClock     = 2 * minutesInHalfClock
+	hoursInHalfClock   = 6
+	hoursInClock       = 2 * hoursInHalfClock
+)
+
 type Point struct {
 	X float64
 	Y float64
@@ -42,17 +51,17 @@ func makeHand(p Point, length float64) Point {
 }
 
 func secondsInRadians(t time.Time) float64 {
-	return math.Pi / (30 / float64(t.Second()))
+	return math.Pi / (secondsInHalfClock / float64(t.Second()))
 }
 
 func minutesInRadians(t time.Time) float64 {
-	return (secondsInRadians(t) / 60) +
-		(math.Pi / (30 / float64(t.Minute())))
+	return (secondsInRadians(t) / secondsInClock) +
+		(math.Pi / (minutesInHalfClock / float64(t.Minute())))
 }
 
 func hoursInRadians(t time.Time) float64 {
-	return (minutesInRadians(t) / 12) +
-		(math.Pi / (6 / float64(t.Hour()%12)))
+	return (minutesInRadians(t) / hoursInClock) +
+		(math.Pi / (hoursInHalfClock / float64(t.Hour()%hoursInClock)))
 }
 
 func secondHandPoint(t time.Time) Point {
