0536a1fdb20a67f1f1b9eecea16af2a459cb3f62
godprobe <godprobe.misc+github@gmail.com>
2023-02-24 23:56:56 -0800

    A failing FS for more test confidence

--- reading_files/blogposts_test.go
+++ reading_files/blogposts_test.go
@@ -1,11 +1,16 @@
 package blogposts_test
 
 import (
+	"errors"
+	"io/fs"
 	"testing"
 	"testing/fstest"
 
 	"blogposts"
 )
+
+type StubFailingFS struct {
+}
 
 func TestNewBlogPost(t *testing.T) {
 	fs := fstest.MapFS{
@@ -22,4 +27,8 @@ func TestNewBlogPost(t *testing.T) {
 	if len(posts) != len(fs) {
 		t.Errorf("got %v posts, wanted %v posts", len(posts), len(fs))
 	}
+}
+
+func (s StubFailingFS) Open(nam string) (fs.File, error) {
+	return nil, errors.New("oh no, I always fail")
 }
