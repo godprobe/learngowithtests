2bea8659dc983012b21521397acfc39d05414fd7
godprobe <godprobe.misc+github@gmail.com>
2023-02-28 23:23:42 -0800

    Testing against a second Post to kill the bad data

--- templating/blogrenderer_test.go
+++ templating/blogrenderer_test.go
@@ -15,6 +15,12 @@ func TestRender(t *testing.T) {
 			Description: "This is a description",
 			Tags:        []string{"go", "tdd"},
 		}
+		aSecondPost = blogrenderer.Post{
+			Title:       "a new world",
+			Body:        "This is another post",
+			Description: "This is another description",
+			Tags:        []string{"tdd", "go", "experiments"},
+		}
 	)
 
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
@@ -27,6 +33,19 @@ func TestRender(t *testing.T) {
 
 		got := buf.String()
 		want := `<h1>hello world</h1>`
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+
+		buf.Reset()
+		err = blogrenderer.Render(&buf, aSecondPost)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got = buf.String()
+		want = `<h1>a new world</h1>`
 		if got != want {
 			t.Errorf("got '%s' want '%s'", got, want)
 		}
