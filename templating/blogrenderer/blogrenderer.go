e1c12ac4913e91e4548261ff0aac5c365c938c1d
godprobe <godprobe.misc+github@gmail.com>
2023-03-01 00:23:05 -0800

    Passing the test; still not great code

--- templating/blogrenderer/blogrenderer.go
+++ templating/blogrenderer/blogrenderer.go
@@ -12,6 +12,8 @@ type Post struct {
 
 func Render(w io.Writer, p Post) error {
 	// write data to w
-	_, err := fmt.Fprintf(w, "<h1>%s</h1>", p.Title)
+	_, err := fmt.Fprintf(w, `<h1>%s</h1>
+<p>%s</p>
+Tags: <ul><li>%s</li><li>%s</li></ul>`, p.Title, p.Description, p.Tags[0], p.Tags[1])
 	return err
 }
