d426a43db07d8853eeab5e7284f5f1805ad2514d
godprobe <godprobe.misc+github@gmail.com>
2023-03-01 00:00:33 -0800

    Refactored tests on my own to use mult. test cases

--- templating/blogrenderer_test.go
+++ templating/blogrenderer_test.go
@@ -2,52 +2,51 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 
 	"blogrenderer"
 )
 
-func TestRender(t *testing.T) {
-	var (
-		aPost = blogrenderer.Post{
+var (
+	cases = []struct {
+		Title       string
+		Body        string
+		Description string
+		Tags        []string
+	}{
+		{
 			Title:       "hello world",
 			Body:        "This is a post",
 			Description: "This is a description",
 			Tags:        []string{"go", "tdd"},
-		}
-		aSecondPost = blogrenderer.Post{
+		},
+		{
 			Title:       "a new world",
 			Body:        "This is another post",
 			Description: "This is another description",
 			Tags:        []string{"tdd", "go", "experiments"},
-		}
-	)
+		},
+	}
+)
 
-	t.Run("it converts a single post into HTML", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		err := blogrenderer.Render(&buf, aPost)
+func TestRender(t *testing.T) {
+	buf := bytes.Buffer{}
 
-		if err != nil {
-			t.Fatal(err)
-		}
+	for _, post := range cases {
+		t.Run("convert a single post into HTML", func(t *testing.T) {
+			buf.Reset()
+			err := blogrenderer.Render(&buf, post)
 
-		got := buf.String()
-		want := `<h1>hello world</h1>`
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
+			if err != nil {
+				t.Fatal(err)
+			}
 
-		buf.Reset()
-		err = blogrenderer.Render(&buf, aSecondPost)
-
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		got = buf.String()
-		want = `<h1>a new world</h1>`
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
-	})
+			got := buf.String()
+			want := fmt.Sprintf("<h1>%s</h1>", post.Title)
+			if got != want {
+				t.Errorf("got '%s' want '%s'", got, want)
+			}
+		})
+	}
 }
