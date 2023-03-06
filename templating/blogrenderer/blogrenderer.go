ac290cabf152ec04a1e285140768bc235425117c
godprobe <godprobe.misc+github@gmail.com>
2023-03-05 19:09:15 -0800

    Not-quite-passing index renderer

--- templating/blogrenderer/blogrenderer.go
+++ templating/blogrenderer/blogrenderer.go
@@ -38,5 +38,16 @@ func (r *PostRenderer) Render(w io.Writer, p Post) error {
 }
 
 func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	indexTemplate := `<ol>{{range .}}<li><a href="post/{{.Title}}">{{.Title}}</a></li>{{end}}</ol>`
+
+	templ, err := template.New("index").Parse(indexTemplate)
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, posts); err != nil {
+		return err
+	}
+
 	return nil
 }
