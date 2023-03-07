a7a4fdd444339c4bfbc29665887e4792af34581f
godprobe <godprobe.misc+github@gmail.com>
2023-03-07 01:18:06 -0800

    external index.gohtml; bad tests if newlines used

--- templating/blogrenderer/blogrenderer.go
+++ templating/blogrenderer/blogrenderer.go
@@ -43,16 +43,16 @@ func (r *PostRenderer) Render(w io.Writer, p Post) error {
 }
 
 func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
-	indexTemplate := `<ol>{{range .}}<li><a href="/posts/{{.SanitizedTitle}}">{{.Title}}</a></li>{{end}}</ol>`
+	// indexTemplate := `<ol>{{range .}}<li><a href="/posts/{{.SanitizedTitle}}">{{.Title}}</a></li>{{end}}</ol>`
 
-	templ, err := template.New("index").Parse(indexTemplate)
-	if err != nil {
-		return err
-	}
+	// templ, err := template.New("index").Parse(indexTemplate)
+	// if err != nil {
+	// 	return err
+	// }
 
-	if err := templ.Execute(w, posts); err != nil {
-		return err
-	}
+	// if err := templ.Execute(w, posts); err != nil {
+	// 	return err
+	// }
 
-	return nil
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
 }
