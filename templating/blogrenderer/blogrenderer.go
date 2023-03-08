01324901b99c19081b72586728d01a20e56f2af5
godprobe <godprobe.misc+github@gmail.com>
2023-03-07 23:41:51 -0800

    md parsing from book; appears to break everything

--- templating/blogrenderer/blogrenderer.go
+++ templating/blogrenderer/blogrenderer.go
@@ -4,7 +4,9 @@ import (
 	"embed"
 	"html/template"
 	"io"
-	"strings"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/parser"
 )
 
 var (
@@ -12,17 +14,9 @@ var (
 	postTemplates embed.FS
 )
 
-type Post struct {
-	Title, Body, Description string
-	Tags                     []string
-}
-
-func (p Post) SanitizedTitle() string {
-	return strings.ToLower(strings.Replace(p.Title, " ", "-", -1))
-}
-
 type PostRenderer struct {
-	templ *template.Template
+	templ    *template.Template
+	mdParser *parser.Parser
 }
 
 func NewPostRenderer() (*PostRenderer, error) {
@@ -31,13 +25,27 @@ func NewPostRenderer() (*PostRenderer, error) {
 		return nil, err
 	}
 
-	return &PostRenderer{templ: templ}, nil
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	parser := parser.NewWithExtensions(extensions)
+
+	return &PostRenderer{templ: templ, mdParser: parser}, nil
 }
 
 func (r *PostRenderer) Render(w io.Writer, p Post) error {
-	return r.templ.ExecuteTemplate(w, "blog.gohtml", p)
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", newPostVM(p, r))
 }
 
 func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
 	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
+
+type postViewModel struct {
+	Post
+	HTMLBody template.HTML
+}
+
+func newPostVM(p Post, r *PostRenderer) postViewModel {
+	vm := postViewModel{Post: p}
+	vm.HTMLBody = template.HTML(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+	return vm
 }
